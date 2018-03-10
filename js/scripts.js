4fca6fece52e6a6f0041b38acd64c372b1852ad1
Giles <gileslewey@gmail.com>
2018-03-09 20:57:16 -0600

    x
    x

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
+  fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script><!--facebook script-->
+
+<!-- Can place script tags with JavaScript files here -->
+<script>
+$(function () {
+$('[data-toggle="tooltip"]').tooltip();
+});
+</script>
+<script>
+$(function(){
+  $("#[item1]").tooltip();
+});
+</script>
+<script>
+$(function(){
+  $("#[item2]").tooltip();
+});
+</script>
+<script>
+$(function(){
+  $("#[item3]").tooltip();
+});
+</script>
+<script type="text/javascript">
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+      });
+    }
+    return false;
+  });
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test
+    (d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);
+      js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}
+      (document, 'script', 'twitter-wjs');</script>
+</script>
