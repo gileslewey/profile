fda46971a5e8a75e68059d16ce74748a4d6e0222
Giles <gileslewey@gmail.com>
2018-03-12 23:20:10 -0500

    3
    3

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,51 @@
 
-<!-- Can place script tags with JavaScript files here -->
+//<!-- Can place script tags with JavaScript files here -->
 //facebook
-<script>(function(d, s, id) {
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
   fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script><!--facebook script-->
+}(document, 'script', 'facebook-jssdk'));
+//<!--facebook script-->
 
 //twitter
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test
     (d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);
-      js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}
-      (document, 'script', 'twitter-wjs');</script>
-</script>
+js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}    (document, 'script', 'twitter-wjs');
+//</script>
 
 //DOM ready
 $(document).ready(function(){
+  //alert("Document is ready!");
 
-
-<script>
+  $("#button").on("click", function() {
+    var comment=$(".message-box").val();
+    $("#visible-comment").html(comment);
+    $(".message-box").hide();
+    return false;
+  });
 //tooltips
-$(function () {
-$('[data-toggle="tooltip"]').tooltip();
-});
-</script>
-<script>
-$(function(){
-  $("#[item1]").tooltip();
-});
-</script>
-<script>
-$(function(){
-  $("#[item2]").tooltip();
-});
-</script>
-<script>
-$(function(){
-  $("#[item3]").tooltip();
-});
-</script>
+//$(function () {
+//$('[data-toggle="tooltip"]').tooltip();
+//});
+
+//$(function(){
+//  $("#[item1]").tooltip();
+//});
+
+//$(function(){
+//  $("#[item2]").tooltip();
+//});
+
+//$(function(){
+//  $("#[item3]").tooltip();
+//});
+
+
 //smooth scrolling
-<script type="text/javascript">
+
   var $root = $('html, body');
   $('.navbar-nav a').click(function() {
     var href = $.attr(this, 'href');
